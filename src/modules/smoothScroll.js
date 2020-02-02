3d711efee89b823c76b6ddf1235f86ec3f6632e7
Aleksey Bat <al-bat-ros@mail.ru>
2020-02-02 16:34:08 +0500

    smoothScroll

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,18 +1,28 @@
 const smoothScroll = () => {
 
-    const menu = document.querySelector('menu');
-   
+    const menu = document.querySelector('menu'),
+          hrefs = document.querySelectorAll('a[href*="#"]');
+
     menu.addEventListener('click', (event) => {
         let target = event.target; 
+        hrefs.forEach(element => {
+            if(element === target && !target.matches('.close-btn')){
+                event.preventDefault(); 
+                const hrefID = target.getAttribute('href');
+                console.log(document.querySelector('' + hrefID));
+                document.querySelector('' + hrefID).scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                }) ;
+            };
+        });    
+    });
+    hrefs[0].addEventListener('click', () => {
         event.preventDefault(); 
-        
-        const hrefID = target.getAttribute('href');
-    
-        document.querySelector('' + hrefID).scrollIntoView({
+        document.querySelector('#service-block').scrollIntoView({
             behavior: "smooth",
             block: "start"
-        }) 
-        
+        });
     });
 }
 export default smoothScroll;
